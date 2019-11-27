cbd059b35c1025ef18a72346f6b9958d6b270484
VIVEKANAND SINGH <vivekanand.singh@niki.ai>
2019-11-28 00:07:28 +0530

    organized imports

--- twitterapi/src/main/java/com/vivek/twitterapi/controller/TwitterController.java
+++ twitterapi/src/main/java/com/vivek/twitterapi/controller/TwitterController.java
@@ -3,7 +3,6 @@ package com.vivek.twitterapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.web.bind.annotation.GetMapping;
